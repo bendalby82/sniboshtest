a4dff7e7093d2500c0d36bcf6479409c9e915218
Dalby <Ben.Dalby@emc.com>
2017-11-16 18:16:46 +0000

    Added URLs and correct curl command

--- test-routers.sh
+++ test-routers.sh
@@ -5,13 +5,17 @@ timestamp(){
 }
 
 LOGFILE=$PWD/test-routers-log.txt
-LB_TEST_APP=https://
-GOROUTER_IPS=(127.0.0.1 127.0.0.2 127.0.0.3)
-CURL_COMMAND=
+LB_TEST_APP=https://statictestapp.max.pepsi.cf-app.com/
+GOROUTER_IPS=(http://10.0.32.15:8080/health http://10.0.0.14:8080/health http://10.0.0.15:8080/health)
+CURL_COMMAND="curl --silent --output /dev/null --write-out '%{http_code}\n' --max-time 1 "
+HTTPS_CURL="$CURL_COMMAND --insecure "
+POLLING_INTERVAL_IN_SECONDS=10
 
-
-for ((i=1;i<=5;i++));
+#Sample for an hour (6 samples a minute, 60 minutes)
+for ((i=1;i<=360;i++));
 do
+
+
 
 	for GOROUTER in ${!GOROUTER_IPS[*]}
 	do
